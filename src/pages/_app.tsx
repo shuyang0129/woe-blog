2703e74a81b9ad45b229eef3d822db582ef598f7
shuyang <shuyang0129@gmail.com>
2023-04-09 12:51:30 +0800

    feat: add next/font with Tailwind CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
+
+import { Inter } from 'next/font/google'
+
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
