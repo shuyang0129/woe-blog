3318f07789e64adcc1ecd0ce1e6403fff3619e55
Shuyang <shuyang0129@gmail.com>
2023-04-13 13:56:19 +0800

    feat: add inter font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,59 @@
-import '@/styles/reset.css'
 import '@/styles/globals.css'
+import '@/styles/reset.css'
 
-import { Inter } from 'next/font/google'
-
-import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
 import { Fragment } from 'react'
 
-const inter = Inter({
-  subsets: ['latin'],
+import type { AppProps } from 'next/app'
+
+const inter = localFont({
+  src: [
+    {
+      path: './fonts/Inter/Inter-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Inter/Inter-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
   variable: '--font-inter',
 })
 
