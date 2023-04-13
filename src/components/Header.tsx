632ceae8551e9d39212c4ba6d823caffac822a87
Shuyang <shuyang0129@gmail.com>
2023-04-13 23:02:15 +0800

    refactor: use clsx to improve readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
+import DarkModeToggler from '@/components/DarkModeToggler'
+import clsx from 'clsx'
 import localFont from 'next/font/local'
-import React, { memo, useState } from 'react'
-
-import DarkModeToggler from './DarkModeToggler'
+import React, { memo } from 'react'
 
 const logo = localFont({ src: '../pages/fonts/Bungee_Shade/BungeeShade-Regular.ttf' })
+
 function Header() {
   return (
-    <header className="bg-white flex items-center justify-center p-6 lg:px-8">
-      <div className="flex-1 flex items-center justify-between max-w-7xl">
-        <h2 className={`${logo.className}`}>ShuYang</h2>
+    <header
+      className={clsx(
+        'bg-white',
+        'dark:bg-zinc-700',
+        'flex',
+        'items-center',
+        'justify-center',
+        'p-6',
+        'lg:px-8',
+        'transition',
+        'duration-200'
+      )}
+    >
+      <div className={clsx('flex-1', 'flex', 'items-center', 'justify-between', 'max-w-7xl')}>
+        <h2 className={clsx(logo.className, 'text-zinc-700', 'dark:text-zinc-100')}>ShuYang</h2>
         <DarkModeToggler />
       </div>
     </header>
