04b400f4986c0156db6b7eccba0996d6ed197e27
Shuyang <shuyang0129@gmail.com>
2023-04-15 16:10:16 +0800

    feat: implement style of blog card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import BlogListSection from '@/components/BlogListSection'
+import Container from '@/components/Container'
 import React from 'react'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <Container.Outer>
+      <BlogListSection />
+    </Container.Outer>
+  )
 }
 
 export default Home
