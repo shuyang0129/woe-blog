5ee20b2594ec78d24b96d1f5d7e9f9655219b695
Shuyang <shuyang0129@gmail.com>
2023-04-15 22:37:46 +0800

    refactor: simplify styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,7 @@ const logo = localFont({ src: '../pages/fonts/Bungee_Shade/BungeeShade-Regular.t
 function Header() {
   return (
     <header>
-      <Container
-        className={clsx('dark:bg-zinc-700', 'p-6', 'lg:px-8', 'transition', 'duration-200')}
-      >
+      <Container className={clsx('py-6')}>
         <div className={clsx('flex-1', 'flex', 'items-center', 'justify-between', 'max-w-7xl')}>
           <h2 className={clsx(logo.className, 'text-zinc-700', 'dark:text-zinc-100')}>ShuYang</h2>
           <DarkModeToggler />
