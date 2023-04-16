2feaf4ac7b8749b35c9149a0e0fb4e294ae49e2c
Shuyang <shuyang0129@gmail.com>
2023-04-16 22:51:51 +0800

    feat: set my own gray colors in light mode and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
       },
+      colors: {
+        gray: {
+          50: 'rgb(var(--color-zinc-50) / <alpha-value>)',
+          100: 'rgb(var(--color-zinc-100) / <alpha-value>)',
+          200: 'rgb(var(--color-zinc-200) / <alpha-value>)',
+          300: 'rgb(var(--color-zinc-300) / <alpha-value>)',
+          400: 'rgb(var(--color-zinc-400) / <alpha-value>)',
+          500: 'rgb(var(--color-zinc-500) / <alpha-value>)',
+          600: 'rgb(var(--color-zinc-600) / <alpha-value>)',
+          700: 'rgb(var(--color-zinc-700) / <alpha-value>)',
+          800: 'rgb(var(--color-zinc-800) / <alpha-value>)',
+          900: 'rgb(var(--color-zinc-950) / <alpha-value>)',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
