97a52ca71a95f6c16a60009fddcb8268299bcc17
Shuyang <shuyang0129@gmail.com>
2023-04-17 15:24:28 +0800

    feat(Home): add missing meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
+import Head from 'next/head'
+import React, { Fragment } from 'react'
+
 import BlogListSection from '@/components/BlogListSection'
 import Container from '@/components/Container'
 import HeroSection from '@/components/HeroSection'
-import React, { Fragment } from 'react'
 
 function Home() {
   return (
     <Fragment>
+      <Head>
+        <title>Code Your Way to Software Engineering | ShuYang</title>
+        <meta
+          name="description"
+          content="A blog covering programming and other topics of interest."
+        />
+      </Head>
       <Container.Outer>
         <HeroSection />
       </Container.Outer>
