d2700c906f3ae25770fe51e6813ed1b1d0aff074
Shuyang <shuyang0129@gmail.com>
2023-04-18 15:06:39 +0800

    feat: make header sticky top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,15 @@ import Logo from '@/components/Logo'
 
 function Header() {
   return (
-    <header>
-      <Container className={clsx('py-6')}>
-        <div className={clsx('flex-1', 'flex', 'items-center', 'justify-between', 'max-w-7xl')}>
-          <Logo />
-          <DarkModeToggler />
-        </div>
-      </Container>
+    <header className={clsx('sticky', 'top-0', 'z-50')}>
+      <Container.Outer>
+        <Container.Inner className={clsx('py-6', 'bg-white', 'dark:bg-zinc-900')}>
+          <div className={clsx('flex-1', 'flex', 'items-center', 'justify-between', 'max-w-7xl')}>
+            <Logo />
+            <DarkModeToggler />
+          </div>
+        </Container.Inner>
+      </Container.Outer>
     </header>
   )
 }
