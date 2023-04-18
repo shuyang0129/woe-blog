477177ba3edf4bf88a07ace34c795b0f288e6284
Shuyang <shuyang0129@gmail.com>
2023-04-18 16:59:44 +0800

    refactor: remove Fragment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,36 +3,28 @@ import Divider from '@/components/Divider'
 import NewsLetter from '@/components/Footer/NewsLetter'
 import SocialConnections from '@/components/Footer/SocialConnections'
 import clsx from 'clsx'
-import { Fragment, memo } from 'react'
+import { memo } from 'react'
 
 function Footer() {
   return (
-    <Fragment>
-      <footer aria-labelledby="footer-heading">
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        <Container className={clsx('pb-8', 'pt-16', 'sm:pt-24', 'lg:pt-32')}>
-          <Divider />
-          <NewsLetter className={clsx()} />
-          <Divider />
-          <div className={clsx('pt-8', 'md:flex', 'md:items-center', 'md:justify-between')}>
-            <SocialConnections className={clsx('md:order-2')} />
-            <p
-              className={clsx(
-                'mt-8',
-                'text-xs',
-                'leading-5',
-                'text-gray-500',
-                'md:order-1 md:mt-0'
-              )}
-            >
-              &copy; 2023 Shuyang Chou. All rights reserved.
-            </p>
-          </div>
-        </Container>
-      </footer>
-    </Fragment>
+    <footer aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <Container className={clsx('pb-8', 'pt-16', 'sm:pt-24', 'lg:pt-32')}>
+        <Divider />
+        <NewsLetter className={clsx()} />
+        <Divider />
+        <div className={clsx('pt-8', 'md:flex', 'md:items-center', 'md:justify-between')}>
+          <SocialConnections className={clsx('md:order-2')} />
+          <p
+            className={clsx('mt-8', 'text-xs', 'leading-5', 'text-gray-500', 'md:order-1 md:mt-0')}
+          >
+            &copy; 2023 Shuyang Chou. All rights reserved.
+          </p>
+        </div>
+      </Container>
+    </footer>
   )
 }
 
